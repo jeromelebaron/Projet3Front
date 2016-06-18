bca852d88260e96b7269e016f43dbf6a1b0a77ce
Jerdopler <lebaronjerome@free.fr>
2016-06-18 18:06:29 +0200

    MAJ front

--- WebDiy/src/main/java/fr/s2re/managedbean/ChartView.java
+++ WebDiy/src/main/java/fr/s2re/managedbean/ChartView.java
@@ -88,39 +88,4 @@ public class ChartView implements Serializable {
         xAxis.setLabel("Région");
     }
 
-    private void createHorizontalBarModel() {
-        horizontalBarModel = new HorizontalBarChartModel();
-
-        ChartSeries boys = new ChartSeries();
-        boys.setLabel("Boys");
-        boys.set("2004", 50);
-        boys.set("2005", 96);
-        boys.set("2006", 44);
-        boys.set("2007", 55);
-        boys.set("2008", 25);
-
-        ChartSeries girls = new ChartSeries();
-        girls.setLabel("Girls");
-        girls.set("2004", 52);
-        girls.set("2005", 60);
-        girls.set("2006", 82);
-        girls.set("2007", 35);
-        girls.set("2008", 120);
-
-        horizontalBarModel.addSeries(boys);
-        horizontalBarModel.addSeries(girls);
-
-        horizontalBarModel.setTitle("Horizontal and Stacked");
-        horizontalBarModel.setLegendPosition("e");
-        horizontalBarModel.setStacked(true);
-
-        Axis xAxis = horizontalBarModel.getAxis(AxisType.X);
-        xAxis.setLabel("Births");
-        xAxis.setMin(0);
-        xAxis.setMax(200);
-
-        Axis yAxis = horizontalBarModel.getAxis(AxisType.Y);
-        yAxis.setLabel("Gender");
-    }
-
 }
